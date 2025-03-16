522fecd873d9a9971d19f4b2e598404863dad401
Abhishek <chaudharyabhishekmp@gmail.com>
2025-03-16 12:24:09 +0530

    signin and code

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -2,12 +2,25 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
 import { Message } from "@/model/User";
+import { messageSchema } from "@/schemas/messageSchema";
 
 export async function POST(request: Request) {
   await dbConnect();
 
   const { username, content } = await request.json();
   try {
+    const result=messageSchema.safeParse({content})
+    if(!result.success){
+      const messageSchemaError=result.error.format().content?._errors||[];
+      return Response.json({
+        success: false,
+        message:
+        messageSchemaError?.length > 0
+            ? messageSchemaError.join(",")
+            : "Invalid quary paramaters",
+      },
+      { status: 400 })
+    }
     const user = await UserModel.findOne({ username });
     if (!user) {
       return Response.json(
--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const messageSchema = z.object({
-  continue: z
+  content: z
     .string()
     .max(300, { message: "Content must be no longer than 300 characters" }),
 });
